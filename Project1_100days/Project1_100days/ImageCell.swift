5ce99471f31c391860b653c85a826357857e2c94
Aleksandr Zubov <alexanderzubov@icloud.com>
2023-01-22 21:49:36 +0100

    feature challenge 3

---
+++ Project1_100days/Project1_100days/ImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCell.swift
+//  Project1_100days
+//
+//  Created by user228564 on 1/22/23.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageLabel: UILabel!
+    
+}
